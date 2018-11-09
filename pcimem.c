dc4912d310aa38a08b9aa18db896e5286e0cbf8f
Jonny Shipton <jonny@myrtlesoftware.com>
2018-11-09 12:02:21 +0000

    demo: Add input file processing

--- pcimem.c
+++ pcimem.c
@@ -15,7 +15,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -30,6 +30,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
@@ -49,57 +50,83 @@
 
 
 int main(int argc, char **argv) {
-	int fd;
-	void *map_base, *virt_addr;
-	uint64_t read_result, writeval, prev_read_result = 0;
-	char *filename;
-	off_t target, target_base;
-	int access_type = 'w';
-	int items_count = 1;
-	int verbose = 0;
-	int read_result_dupped = 0;
-	int type_width;
-	int i;
-	int map_size = 4096UL;
+    int fd;
+    void *map_base, *virt_addr;
+    uint64_t read_result, writeval, prev_read_result = 0;
+    char *filename;
+    off_t target, target_base;
+    int access_type = 'w';
+    int items_count = 1;
+    int verbose = 1;
+    int read_result_dupped = 0;
+    int type_width;
+    int i;
+    int map_size = 4096UL;
 
-	if(argc < 3) {
-		// pcimem /sys/bus/pci/devices/0001\:00\:07.0/resource0 0x100 w 0x00
-		// argv[0]  [1]                                         [2]   [3] [4]
-		fprintf(stderr, "\nUsage:\t%s { sysfile } { offset } [ type*count [ data ] ]\n"
-			"\tsys file: sysfs file for the pci resource to act on\n"
-			"\toffset  : offset into pci memory region to act upon\n"
-			"\ttype    : access operation type : [b]yte, [h]alfword, [w]ord, [d]ouble-word\n"
-			"\t*count  : number of items to read:  w*100 will dump 100 words\n"
-			"\tdata    : data to be written\n\n",
-			argv[0]);
-		exit(1);
-	}
-	filename = argv[1];
-	target = strtoul(argv[2], 0, 0);
+    FILE *input_fp;
+    bool processing_file = false;
+    char *input_filename;
+    size_t len;
+    ssize_t read;
+    char *line = NULL;
+    char userop[] = "expect"; // longest valid op
+    uint64_t userop_addr = 0, userop_val = 0;
+    uint64_t expect_fail_count = 0;
 
-	if(argc > 3) {
-		access_type = tolower(argv[3][0]);
-		if (argv[3][1] == '*')
-			items_count = strtoul(argv[3]+2, 0, 0);
-	}
+    if(argc == 4 && !strcmp(argv[2], "-f")) {
+        fprintf(stderr, "\nFile Processing Mode\n\n");
+        processing_file = true;
+        filename = argv[1];
+        input_filename = argv[3];
+    }
+    else {
+        if(argc < 3) {
+            // pcimem /sys/bus/pci/devices/0001\:00\:07.0/resource0 0x100 w 0x00
+            // argv[0]  [1]                                         [2]   [3] [4]
+            fprintf(stderr, "\nUsage:\t%s { sysfile } { offset } [ type*count [ data ] ]\n"
+                    "\tsys file: sysfs file for the pci resource to act on\n"
+                    "\toffset  : offset into pci memory region to act upon\n"
+                    "\ttype    : access operation type : [b]yte, [h]alfword, [w]ord, [d]ouble-word\n"
+                    "\t*count  : number of items to read:  w*100 will dump 100 words\n"
+                    "\tdata    : data to be written\n\n"
+                    "\n"
+                    "Usage:\t%s { sysfile } { inputfile }\n"
+                    "\tsys file: sysfs file for the pci resource to act on\n"
+                    "\tinput file: source file containing data to be written in \"operation 0xaddress [0xvalue]\",\n"
+                    "\t\tline-separated format. This supports 32-bit values only. Operations: read, write, expect.\n"
+                    "\t\tE.g. read 0x08f00000\n"
+                    "\t\t     write 0x08f00000 0x0fa1afe1\n"
+                    "\t\t     expect 0x08f00000 0x0fa1afe1\n\n",
+                    argv[0], argv[0]);
+            exit(1);
+        }
+        filename = argv[1];
+        target = strtoul(argv[2], 0, 0);
 
-        switch(access_type) {
-		case 'b':
-			type_width = 1;
-			break;
-		case 'h':
-			type_width = 2;
-			break;
-		case 'w':
-			type_width = 4;
-			break;
-                case 'd':
-			type_width = 8;
-			break;
-		default:
-			fprintf(stderr, "Illegal data type '%c'.\n", access_type);
-			exit(2);
-	}
+        if(argc > 3) {
+            access_type = tolower(argv[3][0]);
+            if (argv[3][1] == '*')
+                items_count = strtoul(argv[3]+2, 0, 0);
+        }
+    }
+
+    switch(access_type) {
+        case 'b':
+            type_width = 1;
+            break;
+        case 'h':
+            type_width = 2;
+            break;
+        case 'w':
+            type_width = 4;
+            break;
+        case 'd':
+            type_width = 8;
+            break;
+        default:
+            fprintf(stderr, "Illegal data type '%c'.\n", access_type);
+            exit(2);
+    }
 
     if((fd = open(filename, O_RDWR | O_SYNC)) == -1) PRINT_ERROR;
     printf("%s opened.\n", filename);
@@ -122,69 +149,142 @@ int main(int argc, char **argv) {
     printf("PCI Memory mapped to address 0x%08lx.\n", (unsigned long) map_base);
     fflush(stdout);
 
-    for (i = 0; i < items_count; i++) {
+    if(processing_file) {
+        if(verbose) fprintf(stderr, "Opening file...\n");
+        if((input_fp = fopen(input_filename, "r")) == NULL)
+            PRINT_ERROR;
+        if(verbose) fprintf(stderr, "Opened file.\n");
 
-        virt_addr = map_base + target + i*type_width;
-        switch(access_type) {
-		case 'b':
-			read_result = *((uint8_t *) virt_addr);
-			break;
-		case 'h':
-			read_result = *((uint16_t *) virt_addr);
-			break;
-		case 'w':
-			read_result = *((uint32_t *) virt_addr);
-			break;
-                case 'd':
-			read_result = *((uint64_t *) virt_addr);
-			break;
-	}
-
-    	if (verbose)
-            printf("Value at offset 0x%X (%p): 0x%0*lX\n", (int) target + i*type_width, virt_addr, type_width*2, read_result);
-        else {
-	    if (read_result != prev_read_result || i == 0) {
-                printf("0x%04X: 0x%0*lX\n", (int)(target + i*type_width), type_width*2, read_result);
-                read_result_dupped = 0;
-            } else {
-                if (!read_result_dupped)
-                    printf("...\n");
-                read_result_dupped = 1;
+        while ((read = getline(&line, &len, input_fp)) != -1) {
+            if(verbose)
+                fprintf(stderr, "Read line: %s", line); // Line will end in \n?
+            // Parse line. Op has max length 6 (expect)
+            if(verbose) fprintf(stderr, "Trying to parse with 3 words...\n");
+            if(sscanf(line, "%6s 0x%lx 0x%lx", userop, &userop_addr, &userop_val) != 3) {
+                // If we don't parse all 3, try parsing a "read" op line
+                strcpy(userop, "read");
+                userop_val = 0;
+                if(verbose) fprintf(stderr, "Trying to parse as read command...\n");
+                if (sscanf(line, "read 0x%lx", &userop_addr) != 1)
+                    PRINT_ERROR;
             }
-        }
-	
-	prev_read_result = read_result;
+            if(verbose) fprintf(stderr, "Parsed.\n");
 
+            // Check the op is valid
+            if(strcmp(userop, "write") && strcmp(userop, "read") && strcmp(userop, "expect")) {
+                fprintf(stderr, "Invalid op: %s\n", userop);
+                PRINT_ERROR;
+            }
+            if(userop_addr >= map_size) {
+                fprintf(stderr, "Address out of bounds: %lx\n", userop_addr);
+                PRINT_ERROR;
+            }
+
+            /* Do the op */
+            if(!strcmp(userop, "write")) {
+                if(verbose)
+                    fprintf(stderr, "Writing to %lx: %lx\n", userop_addr, userop_val);
+                // Write the value
+                virt_addr = map_base + userop_addr;
+                *((uint32_t *) virt_addr) = userop_val;
+            }
+            else {
+                if(verbose)
+                    fprintf(stderr, "Reading from %lx\n", userop_addr);
+                // Read the value
+                virt_addr = map_base + userop_addr;
+                read_result = *((uint32_t *) virt_addr);
+
+                // Check expected value, if required
+                if(!strcmp(userop, "expect")) {
+                    if(read_result == userop_val) {
+                        if(verbose)
+                            fprintf(stderr, "Read expected value\n");
+                    }
+                    else {
+                        fprintf(stderr, "Read UNEXPECTED value: %ld (expected %lx)\n", read_result, userop_val);
+                        ++expect_fail_count;
+                    }
+                }
+                else {
+                    // userop == "read"
+                    fprintf(stdout, "%lx %lx\n", userop_addr, read_result);
+                }
+            }
+            fprintf(stderr, "\n");
+        }
+
+        fclose(input_fp);
+        // getline requires we free the buffer even on failure
+        if (line)
+            free(line);
+    }
+    else {
+        /* Handle reads/writes specificed directly on the CLI */
+        for (i = 0; i < items_count; i++) {
+
+            virt_addr = map_base + target + i*type_width;
+            switch(access_type) {
+                case 'b':
+                        read_result = *((uint8_t *) virt_addr);
+                        break;
+                case 'h':
+                        read_result = *((uint16_t *) virt_addr);
+                        break;
+                case 'w':
+                        read_result = *((uint32_t *) virt_addr);
+                        break;
+                case 'd':
+                        read_result = *((uint64_t *) virt_addr);
+                        break;
+            }
+
+            if (verbose)
+                printf("Value at offset 0x%X (%p): 0x%0*lX\n", (int) target + i*type_width, virt_addr, type_width*2, read_result);
+            else {
+                if (read_result != prev_read_result || i == 0) {
+                    printf("0x%04X: 0x%0*lX\n", (int)(target + i*type_width), type_width*2, read_result);
+                    read_result_dupped = 0;
+                } else {
+                    if (!read_result_dupped)
+                        printf("...\n");
+                    read_result_dupped = 1;
+                }
+            }
+
+            prev_read_result = read_result;
+
+        }
+
+        fflush(stdout);
+
+        if(argc > 4) {
+            writeval = strtoull(argv[4], NULL, 0);
+            switch(access_type) {
+                case 'b':
+                    *((uint8_t *) virt_addr) = writeval;
+                    read_result = *((uint8_t *) virt_addr);
+                    break;
+                case 'h':
+                    *((uint16_t *) virt_addr) = writeval;
+                    read_result = *((uint16_t *) virt_addr);
+                    break;
+                case 'w':
+                    *((uint32_t *) virt_addr) = writeval;
+                    read_result = *((uint32_t *) virt_addr);
+                    break;
+                case 'd':
+                    *((uint64_t *) virt_addr) = writeval;
+                    read_result = *((uint64_t *) virt_addr);
+                    break;
+            }
+            printf("Written 0x%0*lX; readback 0x%*lX\n", type_width,
+                   writeval, type_width, read_result);
+            fflush(stdout);
+        }
     }
 
-    fflush(stdout);
-
-	if(argc > 4) {
-		writeval = strtoull(argv[4], NULL, 0);
-		switch(access_type) {
-			case 'b':
-				*((uint8_t *) virt_addr) = writeval;
-				read_result = *((uint8_t *) virt_addr);
-				break;
-			case 'h':
-				*((uint16_t *) virt_addr) = writeval;
-				read_result = *((uint16_t *) virt_addr);
-				break;
-			case 'w':
-				*((uint32_t *) virt_addr) = writeval;
-				read_result = *((uint32_t *) virt_addr);
-				break;
-			case 'd':
-				*((uint64_t *) virt_addr) = writeval;
-				read_result = *((uint64_t *) virt_addr);
-				break;
-		}
-		printf("Written 0x%0*lX; readback 0x%*lX\n", type_width,
-		       writeval, type_width, read_result);
-		fflush(stdout);
-	}
-
-	if(munmap(map_base, map_size) == -1) PRINT_ERROR;
+    if(munmap(map_base, map_size) == -1) PRINT_ERROR;
     close(fd);
     return 0;
 }
